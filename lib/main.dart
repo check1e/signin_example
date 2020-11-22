6946b52ae81076ac4ec6b4da751728fb392a27ee
ml <30815731+slimlime@users.noreply.github.com>
2020-11-22 10:18:11 +1000

    Add WelcomeScreen widget code

--- lib/main.dart
+++ lib/main.dart
@@ -94,3 +94,14 @@ class _SignUpFormState extends State<SignUpForm> {
     );
   }
 }
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
